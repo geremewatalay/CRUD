b5e6ce284d9960ecd9e481c56da134427a04de51
geremewatalay <geremewatalay@gmail.com>
2024-11-18 08:21:45 -0800

    Commit Table

--- Client/src/Users.jsx
+++ Client/src/Users.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Users() {
+  const [users,setUsers] = useState([{
+    Name:"Geremew",Email:"geremewatalay@gmail.com", Age:20
+  }])
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-3">
-        <thead className="table">
+        <Link to="/create" className="btn btn-success">Add +</Link>
+        <table className="table">
+          <thead>
           <tr>
             <th>Name</th>
             <th>Email</th>
             <th>Age</th>
             <th>Action</th>
           </tr>
-        </thead>
+          </thead>
+         <tbody>
+          {
+            users.map((user) =>{
+              return <tr>
+                <td>{user.Name}</td>
+                <td>{user.Email}</td>
+                <td>{user.Age}</td>
+                <td><button>Edit</button><button>Delete</button></td>
+              </tr>
+            })
+          }
+         </tbody>
+        </table>
       </div>
     </div>
   )
