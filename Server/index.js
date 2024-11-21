7967879acbd4c7f1d8787813382f46f2231cf0db
geremewatalay <geremewatalay@gmail.com>
2024-11-21 00:58:33 -0800

    add put and delete on the server

--- Server/index.js
+++ Server/index.js
@@ -14,11 +14,39 @@ mongoose
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Error connecting to MongoDB:', err));
 
+  app.get('/',(req,res) => {
+    UserModel.find({})
+    .then(users => res.json(users))
+    .catch(err => err.json(err))
+  })
 
+  app.get('/getUser/:id', (req,res)=>{
+    const id  = req.params.id;
+    UserModel.findById({_id:id})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+  })
+
+  app.put('/updateUser/:id', (req,res)=>{
+    const id = req.params.id;
+    UserModel.findByIdAndUpdate({_id: id}, {
+      name:req.body.name,
+      email:req.body.email,
+      age:req.body.age})
+      .then(users => res.json(users))
+      .catch(err => res.json(err))
+  })
+
+  app.delete('/deleteUser/:id', (req ,res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndDelete({_id:id})
+    .then(res => res.json(res))
+    .catch(err => res.json(err))
+  })
 app.post("/createUser",(req,res)=>{
     UserModel.create(req.body)
     .then(users=> res.json(users))
-    .catch(err => res.json(err))
+    .catch(err => err.json(err))
 })
 
 app.listen(3001,()=>{
