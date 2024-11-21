ee186998af0244e31e0e75c56793f94590a7bb80
geremewatalay <geremewatalay@gmail.com>
2024-11-21 00:57:13 -0800

    update and delete commit

--- Client/src/Users.jsx
+++ Client/src/Users.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Users() {
-  const [users,setUsers] = useState([{
-    Name:"Geremew",Email:"geremewatalay@gmail.com", Age:20
-  }])
+  const [users,setUsers] = useState([])
+
+  useEffect(()=>{
+    axios.get('http://localhost:3001')
+    .then(result => setUsers(result.data))
+    .catch(err => console.log(err))
+  },[])
+
+  const handleDelete = (id) => {
+    axios.delete('http://localhost:3001/deleteUser/'+id)
+    .then(res => {console.log(res)
+      window.location.reload()})
+    .catch(err => console.log(err))
+  }
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-3">
@@ -22,12 +34,12 @@ function Users() {
           {
             users.map((user) =>{
               return <tr>
-                <td>{user.Name}</td>
-                <td>{user.Email}</td>
-                <td>{user.Age}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.age}</td>
                 <td>
-                <Link to="/update" className="btn btn-success">Update</Link>
-                  <button>Delete</button>
+                <Link to={`/update/${user._id}`} className="btn btn-success">Update</Link>
+                  <button className="btn btn-danger" onClick={(e) => handleDelete(user._id)}>Delete</button>
                 </td>
               </tr>
             })
